f3e6075306391b6764620faee3ae7367d04569ca
Archit Gupta <rarchitgupta@gmail.com>
2020-09-21 23:15:26 +0530

    Updates

---
+++ precache-manifest.2c492b47e269eec0a90b2e3eebd567c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42574481f131d329779592d9e698943a",
+    "url": "/randomquotegenerator/index.html"
+  },
+  {
+    "revision": "00f4bc3b33d1f5eb690e",
+    "url": "/randomquotegenerator/static/css/main.588859da.chunk.css"
+  },
+  {
+    "revision": "46fe6daa97105ebf9f06",
+    "url": "/randomquotegenerator/static/js/2.dcfeed20.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/randomquotegenerator/static/js/2.dcfeed20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00f4bc3b33d1f5eb690e",
+    "url": "/randomquotegenerator/static/js/main.bc2090d2.chunk.js"
+  },
+  {
+    "revision": "0197f7fec09050af0b81",
+    "url": "/randomquotegenerator/static/js/runtime-main.bdaa743f.js"
+  }
+]);
